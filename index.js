25641b093ed2d455efda92f6d5cf92a78656b363
dimaboi21 <boidima21@gmail.com>
2023-05-07 12:08:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const todoInput = document.querySelector(".todo__input");
+const addBtn = document.querySelector(".todo__btn");
+const todoList = document.querySelector(".todo__list");
+
+const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+savedTodos.forEach((todo) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <input type="checkbox" ${todo.completed ? "checked" : ""}/>
+    <span>${todo.value}</span>
+    <button>Видалити</button>
+    `;
+  todoList.appendChild(li);
+});
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (todoInput.value !== "") {
+    const todo = {
+        text:todoInput.value,
+        completed:false
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <input type="checkbox" />
+    <span>${todoInput.value}</span>
+    <button>Видалити</button>
+    `;
+
+    todoList.appendChild(li);
+    savedTodos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+    todoInput.value = "";
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const li =e.target.preventNode;
+    const index = Array.prototype.indexOf.call(todoList.children, li);
+    savedTodos.savedTodos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+    li.remove();
+  } else if (e.target.tagName === "INPUT") {
+    const li = e.target.preventNode;
+    const index = Array.prototype.indexOf.call(todolist.shildren, li);
+    savedTodos [index].completed = e.target.checked;
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+  }
+});
